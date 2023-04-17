8cdfb4c9732ceab3d51bd8c2a0e6e07f4532febf
Marie <mari.plisson@gmail.com>
2023-04-17 17:20:27 +0200

    terre06

--- terre06.py
+++ terre06.py
@@ -1,2 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
+if len(sys.argv) < 2:
+    print("Erreur: veuillez entrer argument valide")
+
+for argument in sys.argv [1:]:
+    if argument.isspace():
+        print("Erreur: veuillez entrer argument valide")
+    else:
+        print(argument[::-1])
+
+
+
+
+
+
